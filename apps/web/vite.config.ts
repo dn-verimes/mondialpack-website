f01d4c42bfa5f7d81a45dfba080429b56fe862ee
daang6 <157671721+daang6@users.noreply.github.com>
2025-05-05 21:56:27 +0200

    chore: update pnpm lockfile

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,6 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // This will expose the server to your local network
+    port: 8083, // Set to your desired port
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
